4a5b238ec699c4dd04c30c8c77720f2f41ca9af0
Michael Fields <michael@mfields.org>
2011-06-09 11:35:37 -0700

    First.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Proto</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" src="<?php print get_stylesheet_uri(); ?>">
+	</head>
+	<body>
+		
+	</body>
+</html>
