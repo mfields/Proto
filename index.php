e2b91211addc68f17e9fad7ce47e4b394c40a769
Michael Fields <michael@mfields.org>
2011-07-13 08:58:06 -0700

    s/src/href - Come on, admit it. You've done this before too :)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>Proto</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" src="<?php print get_stylesheet_uri(); ?>">
+		<link rel="stylesheet" href="<?php print get_stylesheet_uri(); ?>">
 	</head>
 	<body>
 		
