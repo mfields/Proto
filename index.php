76b33b82a8e8fb0a6ee1931f46772fb7c7c8878a
Michael Fields <michael@mfields.org>
2012-12-14 18:49:50 -0800

    Add wp_head() and wp_footer().

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 		<title>Proto</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="<?php print get_stylesheet_uri(); ?>">
+		<?php wp_head(); ?>
 	</head>
 	<body>
-		
+		<?php wp_footer(); ?>
 	</body>
 </html>
